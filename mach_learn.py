705c6ed35342c69a84a8cafb6ddb7656246f3575
Bruno Leardini <bruno.elrib@gmail.com>
2023-10-16 09:32:44 -0300

    Commit V!

---
+++ mach_learn.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+base = pd.read_excel("Horas Sprint - Boty.xlsx")
+
+plt.scatter(base["Horas Planejadas"], base["Horas Gastas"])
+
+# Adicione títulos aos eixos X e Y
+plt.xlabel("Horas Planejadas")  # Título do eixo X
+plt.ylabel("Horas Gastas")  # Título do eixo Y
+
+reg = linear_model.LinearRegression()
+
+reg.fit(base["Horas Planejadas"].values.reshape(-1, 1), base["Horas Gastas"])
+
+user_input = input("Digite o valor de horas a serem planejadas: ")
+input_value = float(user_input)  # Converte a entrada do usuário em um float
+
+# Faça a previsão usando o modelo treinado com o valor inserido pelo usuário
+prediction = reg.predict([[input_value]])
+
+plt.scatter(base["Horas Planejadas"], base["Horas Gastas"])
+plt.scatter(input_value, prediction[0], color="k")
+x = np.array(base["Horas Planejadas"])
+y = reg.intercept_ + x * reg.coef_
+plt.plot(x, y, "r")
+plt.show()
+
+print(f'A previsão para o valor inserido é: {prediction[0]} horas gastas')
---
+++ mach_learn2.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+# Carregue o arquivo Excel existente
+base = pd.read_excel("Horas Sprint - Boty.xlsx")
+
+# Plote os dados
+plt.scatter(base["Horas Gastas"], base["Horas Planejadas"])
+plt.xlabel("Horas Gastas")
+plt.ylabel("Horas Planejadas")
+
+# Crie um modelo de regressão linear
+reg = linear_model.LinearRegression()
+reg.fit(base["Horas Gastas"].values.reshape(-1, 1), base["Horas Planejadas"])
+
+# Solicite ao usuário um valor de horas a serem gastas
+user_input = input("Digite o valor de horas a serem gastas: ")
+input_value = float(user_input)
+
+# Faça a previsão com base no valor inserido pelo usuário
+prediction = reg.predict([[input_value]])
+
+# Plote o resultado
+plt.scatter(base["Horas Gastas"], base["Horas Planejadas"])
+plt.scatter(input_value, prediction[0], color="k")
+x = np.array(base["Horas Gastas"])
+y = reg.intercept_ + x * reg.coef_
+plt.plot(x, y, "r")
+plt.show()
+
+print(f'A previsão para o valor inserido é: {prediction[0]} horas planejadas')
+
+# Carregue o arquivo Excel resultante (se existir)
+try:
+    result_df = pd.read_excel("Horas_Sprint_Resultado.xlsx")
+except FileNotFoundError:
+    result_df = pd.DataFrame()
+
+# Verifique se o valor já está presente no arquivo resultante
+if input_value not in result_df["Horas Gastas"].values:
+    # Crie um novo DataFrame com as informações de horas gastas e previsão de horas planejadas
+    data = {'Horas Gastas': [input_value],
+            'Horas Planejadas': [prediction[0]]}
+
+    # Crie um DataFrame a partir do dicionário 'data'
+    new_data = pd.DataFrame(data)
+
+    # Concatene o novo DataFrame com o DataFrame resultante
+    result_df = pd.concat([result_df, new_data], ignore_index=True)
+
+    # Exporte o DataFrame atualizado para um arquivo Excel
+    result_df.to_excel("Horas_Sprint_Resultado.xlsx", index=False)
